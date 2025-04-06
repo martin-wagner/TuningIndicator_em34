b4894c84fe01e104064a29783a68c1542a566822
Martin Wagner <martin.wagner@kaltensondheim.de>
2025-04-06 11:04:07 +0200

    Formatter commit

--- Code/Firmware/eyetube/src/main.cpp
+++ Code/Firmware/eyetube/src/main.cpp
@@ -1,7 +1,7 @@
 /*
-6U5 Digital Replica by retronics.no
-EM34 Digital Replica by Martin Wagner DL2WAG
-*/
+ 6U5 Digital Replica by retronics.no
+ EM34 Digital Replica by Martin Wagner DL2WAG
+ */
 
 #include <Arduino.h> 
 #include <Arduino_GFX_Library.h> //https://github.com/moononournation/Arduino_GFX | GFX Library for Arduino@^1.4.7
@@ -33,13 +33,14 @@ static constexpr uint8_t TYPE_EM34 = 1;
 #define BRIGHTNESS 255 // Max 255
 
 Arduino_DataBus *bus = new Arduino_HWSPI(PIN_DC, PIN_CS);
-Arduino_GFX *gfx = new Arduino_GC9107(bus, PIN_RES, 0 /* rotation */, true /* IPS */, GC9107_TFTWIDTH, GC9107_TFTHEIGHT, 0, 0, 0, 0);
+Arduino_GFX *gfx = new Arduino_GC9107(bus, PIN_RES, 0 /* rotation */,
+    true /* IPS */, GC9107_TFTWIDTH, GC9107_TFTHEIGHT, 0, 0, 0, 0);
 HardwareSerial uart2(PA3, PA2);
 uint8_t oldHalfAngleBottom;
 uint8_t oldHalfAngleTop;
 float agcFiltered;
-uint8_t fadeInCounter=0;
-uint32_t fadeInLastChange=millis();
+uint8_t fadeInCounter = 0;
+uint32_t fadeInLastChange = millis();
 uint32_t fade = 0;
 uint8_t type = 0;
 
@@ -57,8 +58,9 @@ void setup(void)
   pinMode(PIN_TYPE2, INPUT_PULLUP);
   pinMode(PIN_TYPE3, INPUT_PULLUP);
   delay(5);
-  type = !digitalRead(PIN_TYPE0) | (!digitalRead(PIN_TYPE1) << 1) | (!digitalRead(PIN_TYPE2) << 2) | (!digitalRead(PIN_TYPE3) << 3);
-  digitalWrite(PIN_BACKLIGHT,LOW);
+  type = !digitalRead(PIN_TYPE0) | (!digitalRead(PIN_TYPE1) << 1)
+      | (!digitalRead(PIN_TYPE2) << 2) | (!digitalRead(PIN_TYPE3) << 3);
+  digitalWrite(PIN_BACKLIGHT, LOW);
 
   switch (type) {
     case TYPE_6U5:
@@ -69,36 +71,34 @@ void setup(void)
       break;
     default:
       uart2.println("Digital Eye Tube replacement. Type reading failed");
-      uart2.println("Supported types: 6U5 (" + String(TYPE_6U5) + "), EM64 (" + String(TYPE_EM34) + "). Found: " + String(type));
+      uart2.println(
+          "Supported types: 6U5 (" + String(TYPE_6U5) + "), EM64 ("
+              + String(TYPE_EM34) + "). Found: " + String(type));
       uart2.println("Reboot...");
       delay(1000);
       NVIC_SystemReset();
   }
 
   //Display startup
-  if (!gfx->begin())
-  {
+  if (!gfx->begin()) {
     uart2.println("gfx->begin() failed!");
     delay(1000);
     NVIC_SystemReset();
   }
   gfx->fillScreen(BLACK);
   //circle
-  for(int16_t i = 0; i < 360; i++)
-  {
-    drawLine(i,GREEN_DARK);
+  for (int16_t i = 0; i < 360; i++) {
+    drawLine(i, GREEN_DARK);
   }
   //left
-  for(int16_t i = 45; i < 135; i++)
-  {
-    drawLine(i,GREEN_MEDIUM);
+  for (int16_t i = 45; i < 135; i++) {
+    drawLine(i, GREEN_MEDIUM);
   }
   drawLine(45, GREEN_BRIGHT);
   drawLine(135, GREEN_BRIGHT);
   //right
-  for(int16_t i = 225; i < 315; i++)
-  {
-    drawLine(i,GREEN_MEDIUM);
+  for (int16_t i = 225; i < 315; i++) {
+    drawLine(i, GREEN_MEDIUM);
   }
   drawLine(225, GREEN_BRIGHT);
   drawLine(315, GREEN_BRIGHT);
@@ -109,14 +109,14 @@ void setup(void)
 void loop()
 {
   //Fade in
-  if((fadeInCounter<BRIGHTNESS)&&(millis()-fadeInLastChange)>(120-(fadeInCounter/4)))
-  {
+  if ((fadeInCounter < BRIGHTNESS)
+      && (millis() - fadeInLastChange) > (120 - (fadeInCounter / 4))) {
     fadeInCounter++;
-    fade = fadeInCounter*fadeInCounter;
-    fadeInLastChange=millis();
+    fade = fadeInCounter * fadeInCounter;
+    fadeInLastChange = millis();
   }
   //Enable
-  if(digitalRead(PIN_EN)) {
+  if (digitalRead(PIN_EN)) {
     analogWrite(PIN_BACKLIGHT, fade);
   } else {
     analogWrite(PIN_BACKLIGHT, 0);
@@ -125,74 +125,73 @@ void loop()
   //AGC measurement, filtering and mapping to display angle
   //-0.00321774x^{3}+0.311724x^{2}-9.33456x+90.9922 (0 ... 17V) low sensitivity (bottom)
   //-0.184343x^{3}+4.50276x^{2}-35.8763x+94.963     (0...7V)    high sensivity (top)
-  uint16_t agcRaw=analogRead(PIN_AGC);
-  agcFiltered=(9.0*agcFiltered+agcRaw)/10.0;
+  uint16_t agcRaw = analogRead(PIN_AGC);
+  agcFiltered = (9.0 * agcFiltered + agcRaw) / 10.0;
   float x = agcFiltered / 1024 * 22.5; //agc voltage [V]
   uint8_t newHalfAngleBottom = 45;
   if (x < 17.0) {
-    newHalfAngleBottom = (uint8_t)((float)45.0 - (-0.00321774*pow(x,3)+0.311724*pow(x,2)-9.33456*x+89) / 2);
+    newHalfAngleBottom = (uint8_t) ((float) 45.0
+        - (-0.00321774 * pow(x, 3) + 0.311724 * pow(x, 2) - 9.33456 * x + 89)
+            / 2);
   }
   uint8_t newHalfAngleTop = 45;
   if (x < 7.0) {
-    newHalfAngleTop = (uint8_t)((float)45.0 -(-0.184343*pow(x,3)+4.50276*pow(x,2)-35.8763*x+94.963) / 2);
+    newHalfAngleTop = (uint8_t) ((float) 45.0
+        - (-0.184343 * pow(x, 3) + 4.50276 * pow(x, 2) - 35.8763 * x + 94.963)
+            / 2);
   }
   //crop
-  newHalfAngleBottom=newHalfAngleBottom>45?45:newHalfAngleBottom;
-  newHalfAngleTop=newHalfAngleTop>45?45:newHalfAngleTop;
+  newHalfAngleBottom = newHalfAngleBottom > 45 ? 45 : newHalfAngleBottom;
+  newHalfAngleTop = newHalfAngleTop > 45 ? 45 : newHalfAngleTop;
 
   //todo remove
   //uart2.println("Raw: " + String(agcFiltered) + ", Volt: " + String(x) + ", Angle Bot: " + String(newHalfAngleBottom) + ", Angle Top: " + String(newHalfAngleTop));
 
   //Display update
-  if(newHalfAngleTop>oldHalfAngleTop)
-  {
-    for(int8_t i=oldHalfAngleTop; i<newHalfAngleTop; i++)
-    {
-      drawLine(135+i,GREEN_MEDIUM);
-      drawLine(225-i,GREEN_MEDIUM);
+  if (newHalfAngleTop > oldHalfAngleTop) {
+    for (int8_t i = oldHalfAngleTop; i < newHalfAngleTop; i++) {
+      drawLine(135 + i, GREEN_MEDIUM);
+      drawLine(225 - i, GREEN_MEDIUM);
     }
-    drawLine(135+newHalfAngleTop,GREEN_BRIGHT);
-    drawLine(225-newHalfAngleTop,GREEN_BRIGHT);
+    drawLine(135 + newHalfAngleTop, GREEN_BRIGHT);
+    drawLine(225 - newHalfAngleTop, GREEN_BRIGHT);
   }
-  if(newHalfAngleTop<oldHalfAngleTop)
-  {
-    for(int8_t i=oldHalfAngleTop; i>newHalfAngleTop; i--)
-    {
-      drawLine(135+i,GREEN_DARK);
-      drawLine(225-i,GREEN_DARK);
+  if (newHalfAngleTop < oldHalfAngleTop) {
+    for (int8_t i = oldHalfAngleTop; i > newHalfAngleTop; i--) {
+      drawLine(135 + i, GREEN_DARK);
+      drawLine(225 - i, GREEN_DARK);
     }
-    drawLine(135+newHalfAngleTop,GREEN_BRIGHT);
-    drawLine(225-newHalfAngleTop,GREEN_BRIGHT);
+    drawLine(135 + newHalfAngleTop, GREEN_BRIGHT);
+    drawLine(225 - newHalfAngleTop, GREEN_BRIGHT);
   }
-  if(newHalfAngleBottom>oldHalfAngleBottom)
-  {
-    for(int8_t i=oldHalfAngleBottom; i<newHalfAngleBottom; i++)
-    {
-      drawLine(315+i,GREEN_MEDIUM);
-      drawLine(405-i,GREEN_MEDIUM);
+  if (newHalfAngleBottom > oldHalfAngleBottom) {
+    for (int8_t i = oldHalfAngleBottom; i < newHalfAngleBottom; i++) {
+      drawLine(315 + i, GREEN_MEDIUM);
+      drawLine(405 - i, GREEN_MEDIUM);
     }
-    drawLine(315+newHalfAngleBottom,GREEN_BRIGHT);
-    drawLine(405-newHalfAngleBottom,GREEN_BRIGHT);
+    drawLine(315 + newHalfAngleBottom, GREEN_BRIGHT);
+    drawLine(405 - newHalfAngleBottom, GREEN_BRIGHT);
   }
-  if(newHalfAngleBottom<oldHalfAngleBottom)
-  {
-    for(int8_t i=oldHalfAngleBottom; i>newHalfAngleBottom; i--)
-    {
-      drawLine(315+i,GREEN_DARK);
-      drawLine(405-i,GREEN_DARK);
+  if (newHalfAngleBottom < oldHalfAngleBottom) {
+    for (int8_t i = oldHalfAngleBottom; i > newHalfAngleBottom; i--) {
+      drawLine(315 + i, GREEN_DARK);
+      drawLine(405 - i, GREEN_DARK);
     }
-    drawLine(315+newHalfAngleBottom,GREEN_BRIGHT);
-    drawLine(405-newHalfAngleBottom,GREEN_BRIGHT);
+    drawLine(315 + newHalfAngleBottom, GREEN_BRIGHT);
+    drawLine(405 - newHalfAngleBottom, GREEN_BRIGHT);
   }
-  oldHalfAngleBottom=newHalfAngleBottom;
-  oldHalfAngleTop=newHalfAngleTop;
+  oldHalfAngleBottom = newHalfAngleBottom;
+  oldHalfAngleTop = newHalfAngleTop;
 }
-
 
 void drawLine(uint16_t angle, uint16_t lineColor)
 {
-  double sinVal = sin(angle*0.0174533);
-  double cosVal = cos(angle*0.0174533);
-  gfx->drawLine(CENTER_X+sinVal*INNER_RADIUS, CENTER_Y+cosVal*INNER_RADIUS, CENTER_X+sinVal*OUTER_RADIUS, CENTER_Y+cosVal*OUTER_RADIUS, lineColor);
-  gfx->drawLine(CENTER_X+sinVal*INNER_RADIUS, CENTER_Y+1+cosVal*INNER_RADIUS, CENTER_X+sinVal*OUTER_RADIUS, CENTER_Y+1+cosVal*OUTER_RADIUS, lineColor);
+  double sinVal = sin(angle * 0.0174533);
+  double cosVal = cos(angle * 0.0174533);
+  gfx->drawLine(CENTER_X + sinVal * INNER_RADIUS,
+      CENTER_Y + cosVal * INNER_RADIUS, CENTER_X + sinVal * OUTER_RADIUS,
+      CENTER_Y + cosVal * OUTER_RADIUS, lineColor);
+  gfx->drawLine(CENTER_X + sinVal * INNER_RADIUS,
+      CENTER_Y + 1 + cosVal * INNER_RADIUS, CENTER_X + sinVal * OUTER_RADIUS,
+      CENTER_Y + 1 + cosVal * OUTER_RADIUS, lineColor);
 }
